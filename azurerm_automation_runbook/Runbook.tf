408fe3dd4e2de1f7ba78513c3d94cb448f34128d
ropy1971 <rodolphe@petrowsky.fr>
2019-02-15 11:14:39 +0100

    Modifications

--- azurerm_automation_runbook/Runbook.tf
+++ azurerm_automation_runbook/Runbook.tf
@@ -1,16 +1,17 @@
 # https://www.terraform.io/docs/providers/azurerm/r/automation_runbook.html 
 
 resource "azurerm_automation_runbook" "example" {
-  name                = "Get-AzureVMTutorial"
-  location            = "${var.Region}"
-  resource_group_name = "${var.ResourceGroupName}"
-  account_name        = "${azurerm_automation_account.automation-account.name}"
-  log_verbose         = "true"
-  log_progress        = "true"
-  description         = "This is an example runbook"
-  runbook_type        = "PowerShell"
+  name                                  = "Get-AzureVMTutorial"
+  location                              = "${var.Region}"
+  resource_group_name                   = "${var.ResourceGroupName}"
+  account_name                          = "${azurerm_automation_account.automation-account.name}"
+  log_verbose                           = "true"
+  log_progress                          = "true"
+  description                           = "This is an example runbook"
+  runbook_type                          = "PowerShell"
+  depends_on                            = ["azurerm_automation_account.automation-account"] 
 
   publish_content_link {
-    uri = "https://raw.githubusercontent.com/ropy1971/azure-terraform/azurerm_automation_runbook/runbooks/Get-AzureVMTutorial.ps1"
+    uri                                 = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-automation-runbook-getvms/Runbooks/Get-AzureVMTutorial.ps1"
   }
 }
--- azurerm_automation_runbook/Terraform.tfvars
+++ azurerm_automation_runbook/Terraform.tfvars
@@ -1,9 +1,9 @@
 Region                                  = "westeurope"
 
-#SubscriptionID                          = "Replace with your own ID"
-#ClientID                                = "Replace with your own ID"
-#ClientSecret                            = "Replace with your own ID"
-#TenantID                                = "Replace with your own ID"
+SubscriptionID                          = "Replace with your own ID"
+ClientID                                = "Replace with your own ID"
+ClientSecret                            = "Replace with your own ID"
+TenantID                                = "Replace with your own ID"
 
 ResourceGroupName                       = "aatesting-rg"
 
