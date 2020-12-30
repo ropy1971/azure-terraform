ea9300561c23e99b7578cc3d61808141dd36cffa
rodolphe petrowsky <rodolphe@snappy-services.fr>
2020-12-30 16:20:04 +0100

    added

---
+++ modules/virtual_network_peering/variables.tf
@@ -0,0 +1,28 @@
+
+### main
+
+variable "resource_group_name" {
+
+  type        = string
+  default     = ""
+  description = ""
+
+}
+
+variable "resource_group_location" {
+
+  type        = string
+  default     = ""
+  description = ""
+
+}
+
+### network
+
+variable "virtual_network_name" {
+
+  type        = string
+  default     = ""
+  description = ""
+
+}
